d29d5d231782a01de59227601511d1fe3e6da9b6
Jim Martens <jim1@live.de>
2011-11-25 20:56:58 +0100

    Implemented userEmail.

--- src/acptemplates/packageFeedback.tpl
+++ src/acptemplates/packageFeedback.tpl
@@ -1,13 +1,36 @@
 {include file='setupWindowHeader'}
 
+{if $errorField}
+	<p class="error">{lang}wcf.acp.package.feedback.error{/lang}</p>
+{/if}
+
 <form method="post" action="index.php?page=Package">
     <fieldset>
         <legend>{lang}wcf.acp.package.feedback{/lang}</legend>
         <div class="inner">
             <p>{lang}wcf.acp.package.feedback.description{/lang}</p>
             
-            <div class="inner">
-               <textarea class="inputText" id="feedback" name="feedback" rows="20" onclick="empty()">{lang}wcf.acp.package.feedback.insert{/lang}</textarea>
+            <div{if $errorField == 'userEmail'} class="errorField"{/if}>
+                <label for="userEmail">{lang}wcf.acp.package.feedback.email{/lang}{if $userEmailOptional} (optional){/if}</label>
+                <input type="text" class="inputText" id="userEmail" name="userEmail" value="{@$userEmail}" />
+            	{if $errorField == 'userEmail'}
+					<p>
+						<img src="{@RELATIVE_WCF_DIR}icon/errorS.png" alt="" />
+						{if $errorType == 'empty'}{lang}wcf.global.error.empty{/lang}{/if}
+						{if $errorType == 'notValid'}{lang}wcf.user.error.email.notValid{/lang}{/if}
+					</p>
+				{/if}
+            </div>
+            
+            <div{if $errorField == 'feedback'} class="errorField"{/if}>
+            	<label for="feedback">{lang}wcf.acp.package.feedback.text{/lang}</label>
+            	<textarea class="inputText" id="feedback" name="feedback" rows="20" onclick="empty()">{@$feedback}</textarea>
+            	{if $errorField == 'feedback'}
+					<p>
+						<img src="{@RELATIVE_WCF_DIR}icon/errorS.png" alt="" />
+						{if $errorType == 'empty'}{lang}wcf.global.error.empty{/lang}{/if}
+					</p>
+				{/if}
             </div>
             
             <input type="hidden" name="queueID" value="{@$queueID}" />
@@ -29,15 +52,10 @@
     //<![CDATA[
     window.onload = function() {
     changeHeight();
+    var text = document.getElementById("feedback");
+    text.focus();
 };
-	var once = false;
-	function empty() {
-		if (once) return;
-		var text = document.getElementById("feedback");
-		text.value = "";
-		once = true;
-	}
-    parent.showWindow(true);
+	parent.showWindow(true);
     parent.setCurrentStep('{lang}wcf.acp.package.step.title{/lang}{lang}wcf.acp.package.step.{if $action == 'rollback'}uninstall{else}{@$action}{/if}.{@$step}{/lang}');
     //]]>
 </script>
--- src/pips/FeedbackPackageInstallationPlugin.class.php
+++ src/pips/FeedbackPackageInstallationPlugin.class.php
@@ -18,30 +18,75 @@ class FeedbackPackageInstallationPlugin extends AbstractPackageInstallationPlugi
     
     /**
      * Complete name of table.
+     *
      * @var String
      */
     protected $completeTableName = '';
     
     /**
      * True if the template was shown yet.
+     *
      * @var boolean
      */
     protected $once = false;
     
     /**
      * Contains the feedback of the user.
+     *
      * @var String
      */
     protected $feedback = '';
     
     /**
+     * Contains the user email.
+     *
+     * @var String
+     */
+    protected $userEmail = '';
+    
+    /**
+     * Contains the name of the error field.
+     *
+     * @var String
+     */
+    protected $errorField = '';
+    
+    /**
+     * Contains the name of the error type.
+     *
+     * @var String
+     */
+    protected $errorType = '';
+    
+    /**
+     * Contains the email.
+     *
+     * @var String
+     */
+    protected $email = '';
+    
+    /**
+     * True if the user email is optional.
+     *
+     * @var boolean
+     */
+    protected $userEmailOptional = true;
+    
+    /**
      * Creates a new FeedbackPackageInstallationPlugin object.
+     *
      * @param PackageInstallationQueue $installation
      */
     public function __construct(PackageInstallationQueue $installation) {
         parent::__construct($installation);
         $this->completeTableName = 'wcf'.WCF_N.'_'.$this->tableName;
         $this->readFormParameters();
+        try {
+            $this->validate();
+        } catch (UserInputException $uie) {
+            $this->errorField = $uie->getField();
+            $this->errorType = $uie->getType();
+        }
     }
     
     /**
@@ -50,19 +95,24 @@ class FeedbackPackageInstallationPlugin extends AbstractPackageInstallationPlugi
     public function install() {
         parent::install();
         $feedbackTag = $this->installation->getXMLTag($this->tagName);
-        $email = StringUtil::trim($feedbackTag['email']);
-        $subject = StringUtil::trim($feedbackTag['cdata']);
-        if (!UserUtil::isValidEmail($email)) return; //checks whether the email is valid or not
+        $this->email = StringUtil::trim($feedbackTag['email']);
+        $this->subject = StringUtil::trim($feedbackTag['cdata']);
+        $this->userEmailOptional = (boolean) intval($feedbackTag['userEmailOptional']);
+        
         //checks whether this is an installation or an update
         if ($this->installation->getAction() == 'install') {
             $sql = 'INSERT INTO '.$this->completeTableName.'
-        				(packageID, email, subject)
+        				(packageID, email, subject, userEmailOptional)
         			VALUES ('.intval($this->installation->getPackageID()).", '".
-                        escapeString($email)."', '".escapeString($subject)."')";
+                        escapeString($this->email)."', '".escapeString($this->subject)."',
+                         ".intval($this->userEmailOptional).')';
             WCF::getDB()->sendQuery($sql);
-        } elseif ($this->installatio->getAction() == 'update') {
+        }
+        elseif ($this->installation->getAction() == 'update') {
             $sql = 'UPDATE '.$this->completeTableName."
-            	SET email = '".escapeString($email)."', subject = '".escapeString($subject)."'
+            	SET email = '".escapeString($this->email)."', subject = '".
+                        escapeString($this->subject)."',
+            		userEmailOptional = ".intval($this->userEmailOptional)."
             	WHERE packageID = ".intval($this->installation->getPackageID());
             WCF::getDB()->sendQuery($sql);
         }
@@ -72,13 +122,13 @@ class FeedbackPackageInstallationPlugin extends AbstractPackageInstallationPlugi
      * @see AbstractPackageInstallationPlugin::uninstall()
      */
     public function uninstall() {
+        $this->readData();
         if ($this->once) {
             $this->sendFeedback();
             parent::uninstall();
             return;
         }
-        WCF::getTPL()->display('packageFeedback');
-        exit;
+        $this->show();
     }
     
     /**
@@ -87,18 +137,83 @@ class FeedbackPackageInstallationPlugin extends AbstractPackageInstallationPlugi
     protected function readFormParameters() {
         if (isset($_POST['once'])) $this->once = (boolean) intval($_POST['once']);
         if (isset($_POST['feedback'])) $this->feedback = StringUtil::trim($_POST['feedback']);
+        if (isset($_POST['userEmail'])) $this->userEmail = StringUtil::trim($_POST['userEmail']);
+    }
+    
+    /**
+     * Reads data from the database.
+     */
+    protected function readData() {
+        $sql = 'SELECT email, subject, userEmailOptional
+        		FROM '.$this->completeTableName.'
+        		WHERE packageID = '.intval($this->installation->getPackageID());
+        $row = WCF::getDB()->getFirstRow($sql);
+        $this->email = StringUtil::trim($row['email']);
+        $this->subject = StringUtil::trim($row['subject']);
+        $this->userEmailOptional = (boolean) StringUtil::trim($row['userEmailOptional']);
+    }
+    
+    /**
+     * Validates the user input.
+     *
+     * @throws UserInputException
+     */
+    protected function validate() {
+        if (empty($this->feedback)) {
+            throw new UserInputException('feedback');
+        }
+        
+        if ($this->userEmailOptional) return;
+        
+        if (empty($this->userEmail)) {
+            throw new UserInputException('userEmail');
+        } elseif (!$this->validateEmail($this->userEmail)) {
+            throw new UserInputException('userEmail', 'notValid');
+        }
+        
+    }
+    
+    /**
+     * Assigns the variables to the template.
+     */
+    protected function assignVariables() {
+        WCF::getTPL()->assign(
+            array(
+                'userEmail' => $this->userEmail,
+                'feedback' => $this->feedback,
+                'errorType' => $this->errorType,
+                'errorField' => $this->errorField,
+                'userEmailOptional' => intval($this->userEmailOptional)
+            )
+        );
+    }
+    
+    /**
+     * Shows the template.
+     */
+    protected function show() {
+        $this->assignVariables();
+        WCF::getTPL()->display('packageFeedback');
+        exit;
+    }
+    
+    /**
+     * Checks whether the given email is valid or not.
+     *
+     * @param String $email
+     */
+    protected function validateEmail($email) {
+        return UserUtil::isValidEmail($email);
     }
     
     /**
      * Sends feedback to the recipient saved in database.
      */
     protected function sendFeedback() {
-        $sql = 'SELECT email, subject
-        		FROM '.$this->completeTableName.'
-        		WHERE packageID = '.intval($this->installation->getPackageID());
-        $row = WCF::getDB()->getFirstRow($sql);
         require_once(WCF_DIR.'lib/data/mail/Mail.class.php');
-        $mail = new Mail($row['email'], $row['subject'], $this->feedback);
+        $header = 'Reply-To: '.$this->userEmail.Mail::$crlf;
+        $mail = new Mail($this->email, $this->subject, $this->feedback);
+        $mail->setHeader($header);
         $mail->send();
     }
 }
